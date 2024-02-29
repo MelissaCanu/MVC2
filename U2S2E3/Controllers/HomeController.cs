bc509ffcd3d1c8da8297a9b8e160d18e8973b56f
MelissaCanu <148777539+MelissaCanu@users.noreply.github.com>
2024-02-29 01:12:25 +0100

    aggiunta action result dettagli in home controller, con view relativa dettagli.cshtml con carosello

--- U2S2E3/Controllers/HomeController.cs
+++ U2S2E3/Controllers/HomeController.cs
@@ -67,19 +67,69 @@ namespace U2S2E3.Controllers
             else
             {
                 // L'utente non è un admin, quindi restituisco la vista standard
-                //espressione lambda utilizzata per filtrare una lista di oggetti in C#:
+                //espressione "lambda" utilizzata per filtrare una lista di oggetti in C#:
                 //prendo prodotto p e restituisco il valore del campo InVetrina -> se è true lo aggiungo alla lista
                 prodotti = prodotti.Where(p => p.InVetrina).ToList();
                 return View(prodotti);
             }
         }
 
-        // GET: TogliDaVetrina con parametro id 
+   
+        //GET: DETTAGLI 
+        public ActionResult Dettagli(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
+            //dichiaro e inizializzo variabile prodotto a null
+            Prodotto prodotto = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                //apro connessione e seleziono tramite query prodotto con id specifico
+                string query = "SELECT * FROM Prodotti WHERE IDArticolo = @IDArticolo";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    //aggiungo il parametro id alla query
+                    command.Parameters.AddWithValue("@IDArticolo", id);
+                    connection.Open();
+                    //eseguo la query e leggo i dati 
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            prodotto = new Prodotto()
+                            {
+                                IDArticolo = Convert.ToInt32(reader["IDArticolo"]),
+                                Nome = reader["Nome"].ToString(),
+                                Prezzo = Convert.ToDecimal(reader["Prezzo"]),
+                                Descrizione = reader["Descrizione"].ToString(),
+                                ImgCopertina = reader["ImgCopertina"].ToString(),
+                                ImgAggiuntiva1 = reader["ImgAggiuntiva1"].ToString(),
+                                ImgAggiuntiva2 = reader["ImgAggiuntiva2"].ToString(),
+                                InVetrina = Convert.ToBoolean(reader["InVetrina"])
+                            };
+                        }
+                    }
+                }
+            }
+            //se il prodotto è null, reindirizzo a Index
+            if (prodotto == null)
+            {
+                return RedirectToAction("Index");
+            }
+            //altrimenti restituisco la vista con il prodotto
+            return View(prodotto);
+        }
+
+
+    //*** NEI MIEI METODI PER LA GESTIONE DEI PRODOTTI DA ADMIN ->
+    // USO IL METODO POST PERCHE' MODIFICO IL DB, OVVERO LO STATO DEI PRODOTTI ***  
+
+    // GET: TogliDaVetrina con parametro id 
+    [HttpPost]
         public ActionResult TogliDaVetrina(int id)
         {
             string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
             SqlConnection connection = new SqlConnection(connectionString);
-            {                   
+            {
                 // Query per settare InVetrina a 0 per l'articolo in base all'id
                 string query = "UPDATE Prodotti SET InVetrina = 0 WHERE IDArticolo = @IDArticolo";
                 try
@@ -90,12 +140,12 @@ namespace U2S2E3.Controllers
                 {
                     throw new Exception(ex.Message);
                 }
-                
+
                 using (SqlCommand command = new SqlCommand(query, connection))
-                {   
+                {
                     //aggiungo il parametro id all'update 
                     command.Parameters.AddWithValue("@IDArticolo", id);
-                    
+
                     command.ExecuteNonQuery();
                 }
             }
@@ -103,6 +153,7 @@ namespace U2S2E3.Controllers
         }
 
         // GET: AddToVetrina con parametro id
+        [HttpPost]
         public ActionResult AddToVetrina(int id)
         {
             string connectionString = ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString.ToString();
@@ -130,8 +181,10 @@ namespace U2S2E3.Controllers
             return RedirectToAction("IndexAdmin");
         }
 
-        
-       
+
+
 
     }
 }
+
+
